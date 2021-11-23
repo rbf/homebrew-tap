d3dc093af7ed34a5fca726cf509ba29364aa579b
rbf <rbf.code@pm.me>
2021-11-23 09:39:37 +0100

    pvm 0.1.0 (new formula)

---
+++ Formula/pvm.rb
@@ -0,0 +1,37 @@
+class Pvm < Formula
+  desc "A polyglot version manager for clojure, elixir, golang, node, php, ruby, and rust"
+  homepage "https://github.com/rbf/pvm"
+  url "https://github.com/rbf/pvm/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "aaf1d824fa9a7e8c717b88af4f0ab5db54b839357c272bfed0eb2e51dbcb9e20"
+  license "MIT"
+
+  def install
+    lib.install Dir["*"]
+    bin.install_symlink lib/name
+  end
+
+  def caveats
+    <<~EOS
+      #{name} is in its version #{version} and currently only works for Zshell
+      and on macOS. Bash and Linux compatibility might come in the future.
+
+      Note that although #{name} leverages Docker to start containers for each
+      environment, it expects Docker to be running in the system and does not
+      start it if missing. If you do not have Docker installed on your machine,
+      please consider e.g. checking out Docker Desktop at
+
+          https://www.docker.com/products/docker-desktop
+
+      Finally, please note that for now the following command needs to be
+      manually added at the end of your .zshrc file to allow #{name} to load
+      environments:
+
+        $(#{name} --setup-zsh-env)
+
+    EOS
+  end
+
+  test do
+    assert_equal "#{name} #{version}\n", shell_output("#{bin}/#{name} --version")
+  end
+end
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Rbf Tap
+# rbf's Homebrew Tap
+
+- `brew install rbf/tap/pvm`: Installs `pvm` CLI, A polyglot version manager for
+  clojure, elixir, golang, node, php, ruby, and rust.
 
 ## How do I install these formulae?
 
@@ -9,3 +12,17 @@ Or `brew tap rbf/tap` and then `brew install <formula>`.
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+### To create a new formula
+
+Check out the following documentation:
+
+- [Formula Cookbook](https://docs.brew.sh/Formula-Cookbook)
+- [Formula Ruby API](https://rubydoc.brew.sh/Formula)
+
+## Other custom taps
+
+For reference, these are some interesting custom Homebrew taps:
+
+- https://github.com/aws/homebrew-tap
+- https://github.com/github/homebrew-bootstrap
